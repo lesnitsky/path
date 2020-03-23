1f776364c54f39a33b30ee899063486dc792acc5
Andrei Lesnitsky <andrei.lesnitsky@gmail.com>
2020-03-23 16:01:50 +0300

    upgarde l_cli dep

--- bin/path.dart
+++ bin/path.dart
@@ -1,36 +1,9 @@
-import 'dart:io';
-
 import 'package:l_cli/cli.dart';
-import 'package:l_cli/command.dart';
-import 'package:path/path.dart';
-
-class PathCommand extends Command {
-  PathCommand() : super('main', 'Adds cwd to path');
-
-  @override
-  Future<void> exec(List<String> args) async {
-    final cdir = Directory.current;
-    final pathToAdd = joinAll([cdir.path, ...args]);
-
-    final home = Platform.environment['HOME'];
-    final zshrc = File(join(home, '.zshrc'));
-
-    if (zshrc.existsSync()) {
-      final w = zshrc.openWrite(mode: FileMode.append);
-      final str = 'export PATH=\$PATH:$pathToAdd';
-      w.write(str);
-      await w.flush();
-      print(str);
-    } else {
-      print('No .zshrc found');
-      exit(1);
-    }
-  }
-}
+import 'package:l_path/l_path.dart';
 
 void main(List<String> args) async {
   final cli = new Cli();
-  cli.add(PathCommand());
+  cli.add(PathCommand()..name = 'main');
 
   cli.exec(args);
 }
